4ebca6b827c1dcbf114abb59f46b51ff7f0216a0
takashi saito <takashisaito@MacBook-Air-ts.local>
2025-03-18 17:16:41 +0900

    editのi18n翻訳

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :username, presence: true
+  validates :username, presence: true, length: { maximum: 10 }
+  validates :bio, length: { maximum: 10 }
 
   has_many :posts, dependent: :destroy
 end
